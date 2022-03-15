10e604b1b70219f93fb5ca70d1b79d9893d431e8
Danilson <danilsonvs04@gmail.com>
2022-03-15 18:51:07 -0300

    New docker image

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,39 +3,49 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+
+const AUTH_ERROR = 1;
 
 class AuthController extends Controller
 {
     public function auth(Request $request)
     {
-        if (!Auth::attempt($request->only('email', 'password'))) {
+        try {
+            if (!Auth::attempt($request->only('email', 'password'))) {
+                throw new Exception('Credencias de login inválidas', AUTH_ERROR);
+            }
+
+            $user = User::where('email', $request['email'])->firstOrFail();
+            $token = $user->createToken('auth_token')->plainTextToken;
+
             return response()->json([
-                'success' => false, 
-                'message' => 'Credencias de login inválidas'
+                'success'      => true,
+                'access_token' => $token,
+                'token_type'   => 'Bearer',
+            ]);
+        } catch (Exception $e) {
+            // Parse only the API errors
+            $message = $e->getCode() == AUTH_ERROR ? $e->getMessage() : 'Erro inesperado';
+
+            return response()->json([
+                'success' => false,
+                'message' => $message
             ]);
         }
-
-        $user = User::where('email', $request['email'])->firstOrFail();
-        $token = $user->createToken('auth_token')->plainTextToken;
-
-        return response()->json([
-            'success' => true,
-            'access_token' => $token,
-            'token_type' => 'Bearer',
-        ]);
     }
 
     public function login()
     {
         return response()->json([
-            'success' => false, 
+            'success' => false,
             'message' => 'Você precisa fazer login',
         ]);
     }
 
-    public function user(Request $request) 
+    public function user(Request $request)
     {
         $user = $request->user();
 
@@ -51,7 +61,7 @@ class AuthController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        
+
         return response()->json([
             'success' => true,
             'message' => 'Fim da sessão',
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return json_encode([
+        'current_time' => date('Y-m-d H:i:s')
+    ]);
 });
